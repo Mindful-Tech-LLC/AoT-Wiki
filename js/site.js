6e2ff068fd241f13730750cba95d76baa61d9d2a
句和一 <alice@aisys.dev>
2023-08-17 21:12:02 -0600

    improved

--- js/site.js
+++ js/site.js
@@ -1,14 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
+    var homeBtn = document.getElementById("homeBtn");
     var abtBtn = document.getElementById("aboutBtn");
     var bizBtn = document.getElementById("businessBtn");
+    var home = document.getElementById("home");
     var abt = document.getElementById("about");
     var biz = document.getElementById("business");
-    abt.style.display = "block";
+    home.style.display = "block";
+    abt.style.display = "none";
     biz.style.display = "none";
+
+    homeBtn.addEventListener("click", function () {
+        home.style.display = "block";
+        abt.style.display = "none";
+        biz.style.display = "none";
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: 'smooth'
+        });
+    });
 
     abtBtn.addEventListener("click", function () {
         abt.style.display = "block";
         biz.style.display = "none";
+        home.style.display = "none";
         window.scrollTo({
             top: document.body.scrollHeight,
             behavior: 'smooth'
@@ -18,6 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
     bizBtn.addEventListener("click", function () {
         abt.style.display = "none";
         biz.style.display = "block";
+        home.style.display = "none";
         window.scrollTo({
             top: document.body.scrollHeight,
             behavior: 'smooth'
